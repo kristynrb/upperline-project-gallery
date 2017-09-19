193ce32e64ebf2187fe6458935f566f02b293793
Kristyn Bryan <kristynrb@gmail.com>
2017-09-19 19:18:21 -0400

    updated route and template for portfolio page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
 if __name__ == "__main__":
     app.run()
